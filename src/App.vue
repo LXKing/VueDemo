178e755fe45cfd310de30de66c3dbaae632185dc
LXKing <849237567@qq.com>
2017-07-06 15:08:22 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,130 @@
 <template>
-  <div id="app">
-    <img src="./assets/zb.jpg">
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<div class="page-swipe" id="startAnimation">
+			<mt-swipe :auto="0" :continuous="false" @change="handleChange">
+				<mt-swipe-item class="slide">1</mt-swipe-item>
+				<mt-swipe-item class="slide">2</mt-swipe-item>
+				<mt-swipe-item class="slide">3</mt-swipe-item>
+			</mt-swipe>
+		</div>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+	export default {
+		name: 'app',
+		components: {
+
+		},
+		data() {
+			return {
+				startAnimationCount: ''
+			}
+		},
+		beforeCreate: function() {
+//			console.group('beforeCreate 创建前状态===============》');
+//			console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+//			console.log("%c%s", "color:red", "data   : " + this.$data); //undefined 
+//			console.log("%c%s", "color:red", "message: " + this.message)
+		},
+		created: function() {
+//			console.group('created 创建完毕状态===============》');
+//			console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+//			console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化 
+//			console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+		},
+		beforeMount: function() {
+//			console.group('beforeMount 挂载前状态===============》');
+//			console.log("%c%s", "color:red", "el     : " + (this.$el)); //已被初始化
+//			console.log(this.$el);
+//			console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化  
+//			console.log("%c%s", "color:red", "message: " + this.message); //已被初始化  
+		},
+		mounted: function() {
+			//this.startAnimationCount = this.getstartAnimationCount();			
+			var aaa = $("#startAnimation");
+			console.log(aaa);			
+			var count = $("#startAnimation").find('mt-swipe-item').length;
+			this.startAnimationCount = count;
+			alert(this.startAnimationCount);
+		},
+		beforeUpdate: function() {
+//			console.group('beforeUpdate 更新前状态===============》');
+//			console.log("%c%s", "color:red", "el     : " + this.$el);
+//			console.log(this.$el);
+//			console.log("%c%s", "color:red", "data   : " + this.$data);
+//			console.log("%c%s", "color:red", "message: " + this.message);
+		},
+		updated: function() {
+//			console.group('updated 更新完成状态===============》');
+//			console.log("%c%s", "color:red", "el     : " + this.$el);
+//			console.log(this.$el);
+//			console.log("%c%s", "color:red", "data   : " + this.$data);
+//			console.log("%c%s", "color:red", "message: " + this.message);
+		},
+		beforeDestroy: function() {
+//			console.group('beforeDestroy 销毁前状态===============》');
+//			console.log("%c%s", "color:red", "el     : " + this.$el);
+//			console.log(this.$el);
+//			console.log("%c%s", "color:red", "data   : " + this.$data);
+//			console.log("%c%s", "color:red", "message: " + this.message);
+		},
+		destroyed: function() {
+//			console.group('destroyed 销毁完成状态===============》');
+//			console.log("%c%s", "color:red", "el     : " + this.$el);
+//			console.log(this.$el);
+//			console.log("%c%s", "color:red", "data   : " + this.$data);
+//			console.log("%c%s", "color:red", "message: " + this.message)
+		},
+		methods: {
+			getstartAnimationCount() {
+				var count = $("#startAnimation").find('mt-swipe-item').length;
+				alert(count);
+				return count;
+			},
+			handleChange(index) {
+				alert(index);
+			}
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		text-align: center;
+		color: #FFFFFF;
+		position: absolute;
+		overflow: hidden;
+	}
+</style>
+<style scoped>
+	.page-swipe {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: #FFFFFF;
+		z-index: 999;
+		text-align: center;
+	}
+	
+	.mint-swipe {
+		color: #fff;
+		font-size: 30px;
+		text-align: center;
+	}
+	
+	.mint-swipe-item {
+		line-height: 200px;
+	}
+	
+	.slide {
+		width: 100%;
+		height: 100%;
+		background-color: #0089dc;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
+import Vuex from 'vuex';
+
+Vue.use(Mint);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  mode: 'history',
   router,
-  template: '<App/>',
-  components: { App }
+  //template: '<App/>',
+  //components: { App }
+  render: h => h(App)
 })
